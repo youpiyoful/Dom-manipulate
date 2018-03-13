75ea54b8a7eb20201e875531f5481706fc63254c
xF1owx <florian.r@codeur.online>
2018-03-13 15:25:55 +0100

    reglage des bouttons en javascript

--- script.js
+++ script.js
@@ -1,36 +1,3 @@
-/*function upSizeText(_class){
-  this.class = _class;
-  this.class.style.fontSize = "4em";
-}
-
-var text = document.querySelector("p");
-text.addEventListener("click", upSizeText(text));
-*/
-/*var text = document.querySelector("p");
-text.addEventListener("click", function (e){
-  if (text.style.fontSize === "50px"){
-    text.style.fontSize = "20px";
-  }
-  else{
-    text.style.fontSize = "50px";
-  }
-})
-
-var rectangle = document.getElementByClassName("rectangle");
-button1 = document.getElementById("bouton1");
-button2 = document.getElementById("bouton2");
-button3 = document.getElementById("bouton3");
-button4 = document.getElementById("bouton4");
-button5 = document.getElementById("bouton5");
-
-button1.addEventListener("click", function(upSizeHeight){
-
-})*/
-
-
-
-
-
 function upSizeText(_selector) {
   this.selector = _selector;
   if (this.selector.style.fontSize === "50px"){
@@ -41,10 +8,94 @@ function upSizeText(_selector) {
   }
 }
 
+
+
+function upSizeHeight(_param){
+  this.param = _param;
+  if (this.param.style.height > "300px") {
+      this.param.style.height = "100px";
+  }
+  else{
+    var temp = this.param.style.height;
+    temp += "10px";
+    this.param.style.height = temp;
+  }
+}
+
+function colorSwitch(_param){
+  this.param = _param;
+  this.param.style.backgroundColor = "lightgreen";
+}
+
+function colorSwitch2(_param){
+  this.param = _param;
+  this.param.style.backgroundColor = "red";
+}
+
+function initial(_param){
+  this.param = _param;
+  this.param.style.backgroundColor = "";
+}
+
+
+function disapear(_param){
+  this.param = _param;
+  this.param.style.backgroundColor = "inherit";
+}
+
+function appear(_param){
+  this.param = _param;
+  this.param.style.backgroundColor = "";
+}
+
+
 var text = document.querySelector("p");
+var rectangle = document.getElementById("rectangle");
+var button1 = document.getElementById("bouton1");
+var button2 = document.getElementById("bouton2");
+var button3 = document.getElementById("bouton3");
+var button4 = document.getElementById("bouton4");
+var button5 = document.getElementById("bouton5");
+var button6 = document.getElementById("bouton6");
+
+
+
 
 text.addEventListener("click", function(e){
   upSizeText(text);
-  });
+});
 
+
+button1.addEventListener("click", function(e){
+  upSizeHeight(rectangle);
+})
+
+
+button2.addEventListener("click", function(event){
+  colorSwitch(rectangle);
+})
+
+
+button3.addEventListener("click", function(event){
+  initial(rectangle);
+})
+
+button5.addEventListener("click", function(event){
+  appear(rectangle);
+})
+button4.addEventListener("click", function(event){
+  disapear(rectangle);
+})
+
+button6.addEventListener("click", function(event){
+  colorSwitch2(rectangle);
+})
+
+
+
+
+
+// button6.addEventListener("click", function (event){
+//   colorSwitch(rectangle);
+// })
 
