d23a89048dad3aef53cca4899cce8292dd200481
youpiyoful <yoanfornari@gmail.com>
2018-03-13 11:32:51 +0100

    modif commentary script

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
-function upSizeText(_class){
+/*function upSizeText(_class){
   this.class = _class;
-  this.class.style.font-size = 4em;
+  this.class.style.fontSize = "4em";
 }
 
-var text = document.getElementByClassName("text");
-var textBig = document.getElementByClassName("text").addEventListener(onclick, upSizeText("text"));
+var text = document.querySelector("p");
+text.addEventListener("click", upSizeText(text));
+*/
+var text = document.querySelector("p");
+text.addEventListener("click", function (e){
+  if (text.style.fontSize === "50px"){
+    text.style.fontSize = "20px";
+  }
+  else{
+    text.style.fontSize = "50px";
+  }
+})
+
+
+/*
+function upSizeText(_selector) {
+  this.selector = _selector;
+  this.selector.style.fontSize = "50px";
+}
+var text = document.querySelector("p");
+
+text.addEventListener("click", upSizeText(text));
+
+*/
